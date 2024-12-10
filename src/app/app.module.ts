f1b128451d3d938535431d02d056295cecfe4fbb
ShayanTahir20 <ShayanTahir20>
2024-12-11 01:57:01 +0500

    comment and time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,114 +45,114 @@ appRouter.put(
   updateUserController
 );
 
-appRouter.get("/auth/activate/:token", activateAccountHandler);
-appRouter.get(
-  "/categories-and-courses",
-  passport.authenticate("jwt", { session: false }),
-  getCategoriesAndCourses
-);
-appRouter.post(
-  "/courses",
-  passport.authenticate("jwt", { session: false }),
-  createCourseHandler
-);
-appRouter.get("/swagger.json", (req, res) => {
-  res.json(swaggerSpec);
-});
+// appRouter.get("/auth/activate/:token", activateAccountHandler);
+// appRouter.get(
+//   "/categories-and-courses",
+//   passport.authenticate("jwt", { session: false }),
+//   getCategoriesAndCourses
+// );
+// appRouter.post(
+//   "/courses",
+//   passport.authenticate("jwt", { session: false }),
+//   createCourseHandler
+// );
+// appRouter.get("/swagger.json", (req, res) => {
+//   res.json(swaggerSpec);
+// });
 
-appRouter.get(
-  "/courses",
-  passport.authenticate("jwt", { session: false }),
-  GetCourses
-);
-appRouter.get(
-  "/courses/:courseId",
-  passport.authenticate("jwt", { session: false }),
-  getCourseDetails
-);
-appRouter.get(
-  "/courses/course-with-progress/:courseId",
-  passport.authenticate("jwt", { session: false }),
-  GetCourseWithProgress
-);
-appRouter.get(
-  "/courses/courseUnique/:courseId",
-  passport.authenticate("jwt", { session: false }),
-  getUniqueCourse
-);
-appRouter.get(
-  "/chapters/:courseId/chapters/:chapterId",
-  passport.authenticate("jwt", { session: false }),
-  getChapterHandler
-);
+// appRouter.get(
+//   "/courses",
+//   passport.authenticate("jwt", { session: false }),
+//   GetCourses
+// );
+// appRouter.get(
+//   "/courses/:courseId",
+//   passport.authenticate("jwt", { session: false }),
+//   getCourseDetails
+// );
+// appRouter.get(
+//   "/courses/course-with-progress/:courseId",
+//   passport.authenticate("jwt", { session: false }),
+//   GetCourseWithProgress
+// );
+// appRouter.get(
+//   "/courses/courseUnique/:courseId",
+//   passport.authenticate("jwt", { session: false }),
+//   getUniqueCourse
+// );
+// appRouter.get(
+//   "/chapters/:courseId/chapters/:chapterId",
+//   passport.authenticate("jwt", { session: false }),
+//   getChapterHandler
+// );
 
-appRouter.post(
-  "/courses/:courseId/upload",
-  passport.authenticate("jwt", { session: false }),
-  upload.single("file"),
-  uploadFile
-);
-appRouter.post(
-  "/courses/:courseId/chapters",
-  passport.authenticate("jwt", { session: false }),
-  createChapterHandler
-);
+// appRouter.post(
+//   "/courses/:courseId/upload",
+//   passport.authenticate("jwt", { session: false }),
+//   upload.single("file"),
+//   uploadFile
+// );
+// appRouter.post(
+//   "/courses/:courseId/chapters",
+//   passport.authenticate("jwt", { session: false }),
+//   createChapterHandler
+// );
 
-appRouter.get(
-  "/purchases",
-  passport.authenticate("jwt", { session: false }),
-  checkPurchase
-);
+// appRouter.get(
+//   "/purchases",
+//   passport.authenticate("jwt", { session: false }),
+//   checkPurchase
+// );
 
-appRouter.get(
-  "/:courseId/chapters/:chapterId",
-  passport.authenticate("jwt", { session: false }),
-  getChapter
-);
-appRouter.get(
-  "/chapters/get-chapter",
-  passport.authenticate("jwt", { session: false }),
-  getChapterData
-);
-appRouter.patch(
-  "/courses/:courseId",
-  passport.authenticate("jwt", { session: false }),
-  updateCourse
-);
+// appRouter.get(
+//   "/:courseId/chapters/:chapterId",
+//   passport.authenticate("jwt", { session: false }),
+//   getChapter
+// );
+// appRouter.get(
+//   "/chapters/get-chapter",
+//   passport.authenticate("jwt", { session: false }),
+//   getChapterData
+// );
+// appRouter.patch(
+//   "/courses/:courseId",
+//   passport.authenticate("jwt", { session: false }),
+//   updateCourse
+// );
 
-appRouter.patch(
-  "/courses/:courseId/chapters/:chapterId",
-  passport.authenticate("jwt", { session: false }),
-  updateChapter
-);
-appRouter.patch(
-  "/courses/:courseId/chapters/:chapterId/publish",
-  passport.authenticate("jwt", { session: false }),
-  publishChapterHandler
-);
-appRouter.patch(
-  "/courses/:id/publish",
-  passport.authenticate("jwt", { session: false }),
-  publishCourseHandler
-);
-appRouter.patch(
-  "/courses/:id/unpublish",
-  passport.authenticate("jwt", { session: false }),
-  unpublishCourseHandler
-);
+// appRouter.patch(
+//   "/courses/:courseId/chapters/:chapterId",
+//   passport.authenticate("jwt", { session: false }),
+//   updateChapter
+// );
+// appRouter.patch(
+//   "/courses/:courseId/chapters/:chapterId/publish",
+//   passport.authenticate("jwt", { session: false }),
+//   publishChapterHandler
+// );
+// appRouter.patch(
+//   "/courses/:id/publish",
+//   passport.authenticate("jwt", { session: false }),
+//   publishCourseHandler
+// );
+// appRouter.patch(
+//   "/courses/:id/unpublish",
+//   passport.authenticate("jwt", { session: false }),
+//   unpublishCourseHandler
+// );
 
-appRouter.patch(
-  "/courses/:courseId/chapters/:chapterId/unpublish",
-  passport.authenticate("jwt", { session: false }),
-  unpublishChapterHandler
-);
+// appRouter.patch(
+//   "/courses/:courseId/chapters/:chapterId/unpublish",
+//   passport.authenticate("jwt", { session: false }),
+//   unpublishChapterHandler
+// );
 
-appRouter.get(
-  "/protected",
-  passport.authenticate("jwt", { session: false }),
-  (req: Request, res: Response) => {
-    res.json({ message: "Protected route accessed", user: req.user });
-  }
-);
+// appRouter.get(
+//   "/protected",
+//   passport.authenticate("jwt", { session: false }),
+//   (req: Request, res: Response) => {
+//     res.json({ message: "Protected route accessed", user: req.user });
+//   }
+// );
 
 export default appRouter;
